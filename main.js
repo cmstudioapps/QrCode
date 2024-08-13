c7ff903b2ebc0e2c34ee58830058a82bb90790e2
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-08-13 19:50:06 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
+
+
 function gerar() {
-   
-   let texto = document.getElementById("texto").value
-let div = document.getElementById("qrcode")
-   
-   div.innerHTML = ""
-   
-   let qrCode = new QRCode(div, {
-      
-      text: texto,
-      width: 200,
-      height: 200
-      
-      
-   })
-   
+    let texto = document.getElementById("texto").value;
+    let div = document.getElementById("qrcode");
+
+    // URL base do seu site
+    let baseUrl = window.location.href;
+
+    // Adiciona o texto ao parâmetro 'qrcode'
+    let urlComParametro = `${baseUrl}?qrcode=${encodeURIComponent(texto)}`;
+
+    div.innerHTML = "";
+
+    let qrCode = new QRCode(div, {
+        text: urlComParametro,
+        width: 200,
+        height: 200
+    });
 }
+
+window.addEventListener("keydown", function(e){
+    if(e.key === "Enter") {
+        gerar();
+    }
+});
+
+function qrcodeScaneado() {
+    alert('QR Code escaneado!');
+    // Aqui você pode executar qualquer outra função
+}
+
+function verificarQrcodeNaUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('qrcode')) {
+        qrcodeScaneado();
+    }
+}
+
+window.onload = verificarQrcodeNaUrl;
